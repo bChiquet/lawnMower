2b3e562a47a1e8da0b061f73a6da80e1a4162e5d
bec <benoit.chiquet@gmail.com>
2015-06-24 21:15:12 +0200

    Adding comments for mower class

--- src/main/java/Mower.java
+++ src/main/java/Mower.java
@@ -6,14 +6,26 @@ public class Mower {
     private int posY;
     private String direction;
     private String orders;
-    private int orderPos = 0;
 
+    /**
+     * Setting the mower's position
+     * @param posX the x-position of the mower
+     * @param posY the y-position of the mower
+     * @param direction the direction the mower is facing
+     * @return self - harmcrest style.
+     */
     public Mower setPosition(int posX, int posY, String direction){
         this.posX = posX;
         this.posY = posY;
         this.direction = direction;
         return this;
     }
+
+    /**
+     * Setting the mower's pile.
+     * @param orders the list of orders
+     * @return self - harmcrest style.
+     */
     public Mower setOrders(String orders){
         this.orders = orders;
         return this;
